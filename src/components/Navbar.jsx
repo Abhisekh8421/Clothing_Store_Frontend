6cc3846c8977c47f864dfc66095d8cfafbb5c75b
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-12-23 23:03:00 +0530

    completed the collections page and added the filters like category,subcategory and sort products

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setvisible] = useState(false);
+  const { setShowsearch } = useContext(ShopContext);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
@@ -28,7 +30,11 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" />
+        <img
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          onClick={() => setShowsearch(true)}
+        />
         <div className="group relative">
           <img src={assets.profile_icon} className="w-5 cursor-pointer" />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
@@ -6,10 +6,16 @@ export const ShopContext = createContext();
 const ShopContextProvider = ({ children }) => {
   const currency = "₹";
   const delivery_fee = 40;
+  const [search, setSearch] = useState("");
+  const [showsearch, setShowsearch] = useState(false);
   const value = {
     products,
     currency,
     delivery_fee,
+    search,
+    setSearch,
+    showsearch,
+    setShowsearch,
   };
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
