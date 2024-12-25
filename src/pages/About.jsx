8d30ec86e98a8f2d13a0d760d7c80fa017f615ae
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-12-25 22:38:14 +0530

    completed the About and Contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className=" text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT "} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} className=" w-full md:max-w-[450px]" />
+        <div className=" flex flex-col justify-center gap-6 md:w-2/4 text-gray-600 ">
+          <p>
+            Welcome to Second Skin, your one-stop shop for trendsetting fashion
+            that feels as good as it looks. Discover stylish clothing, curated
+            for every occasion, with seamless shopping and secure payments.
+            Redefine your wardrobe with Second Skin—where comfort meets style!
+          </p>
+          <p>
+            From classic essentials to bold statement pieces, we bring you a
+            collection designed to celebrate your unique style. Shop
+            effortlessly, stay on trend, and let us be part of your fashion
+            journey!
+          </p>
+          <b className=" text-gray-800">Our Mission</b>
+          <p>
+            Our mission is to redefine your shopping experience by offering
+            high-quality, stylish, and affordable clothing that fits every
+            occasion
+          </p>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className=" text-center text-2xl pt-10 border-t ">
+        <Title text1={"CONTACT"} text2={" US"} />
+      </div>
+      <div className=" my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 ">
+        <img src={assets.contact_img} className=" w-full md:max-w-[480px]" />
+        <div className=" flex flex-col justify-center items-start gap-6">
+          <p className=" font-semibold text-xl text-gray-600">Our Store</p>
+          <p className=" text-gray-500">
+            Visakhapatnam <br /> Andhrapradesh, India
+          </p>
+          <p className=" text-gray-600">
+            PhNo:+917330664923 <br /> Email: abhisekhsuru@gamil.com
+          </p>
+          <p className=" font-semibold text-xl text-gray-700">
+            Careers at Forever.
+          </p>
+          <p className=" text-gray-500">Learn more about our teams.</p>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
